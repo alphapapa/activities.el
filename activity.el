ab283c95ad68be26fd67ec5d8bc195149f533506
Adam Porter <adam@alphapapa.net>
2024-01-24 02:22:23 -0600

    Update: Readme, commentary

--- activity.el
+++ activity.el
@@ -20,6 +20,11 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+;;; Acknowledgments:
+
+;; Thanks to Pat Metheny, whose music aided the initial
+;; implementation.
+
 ;;; Commentary:
 
 ;; Inspired by Genera's and KDE's concepts of "activities", this
