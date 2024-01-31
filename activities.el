ee005f6b6b78aab4253f1980c23f9da86257c7ae
Adam Porter <adam@alphapapa.net>
2024-01-30 20:02:10 -0600

    Release: v0.3.2

--- activities.el
+++ activities.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Adam Porter <adam@alphapapa.net>
 ;; URL: https://github.com/alphapapa/activities.el
 ;; Keywords: convenience
-;; Version: 0.3.2-pre
+;; Version: 0.3.2
 ;; Package-Requires: ((emacs "29.1") (persist "0.6"))
 
 ;; This program is free software; you can redistribute it and/or modify
